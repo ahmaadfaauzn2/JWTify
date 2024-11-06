fd3ae49d340e6854bcd2daf078b8b4bcfdab83a9
Ahmad Fauzan <134770763+ahmaadfaauzn2@users.noreply.github.com>
2024-11-06 20:16:19 +0700

    Update SecurityConfig.java

--- src/main/java/com/example/hometestnew/jwt/SecurityConfig.java
+++ src/main/java/com/example/hometestnew/jwt/SecurityConfig.java
@@ -10,7 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @Configuration
@@ -22,12 +26,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .requestMatchers("/login", "/registration", "/profile", "/profile/update", "/profile/image", "/banner", "/services", "/balance", "/topup", "/transaction", "/transaction/history", "/v1/api-docs", "/v2/api-docs", "/v3/api-docs","/v3/api-docs/**", "/swagger-resources", "/swagger-resouirces/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/auth-controller/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf(csrf -> csrf.disable());
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/registration", "/profile", "/profile/update", "/profile/image",
+                                "/banner", "/services", "/balance", "/topup", "/transaction", "/transaction/history",
+                                "/v1/api-docs", "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
+                                "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
+                                "/webjars/**", "/swagger-ui.html", "/auth-controller/**", "/error")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(customCorsConfigurationSource()));
+
         return http.build();
+    }
+
+    
+    private CorsConfigurationSource customCorsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("https://hometestjavaprogrammer-production.up.railway.app");
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
