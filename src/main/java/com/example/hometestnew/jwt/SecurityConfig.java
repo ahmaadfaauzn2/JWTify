85237faf21029dbab56ad00519f81781fe9fe40b
Ahmad Fauzan <134770763+ahmaadfaauzn2@users.noreply.github.com>
2024-11-06 20:25:31 +0700

    Update SecurityConfig.java

--- src/main/java/com/example/hometestnew/jwt/SecurityConfig.java
+++ src/main/java/com/example/hometestnew/jwt/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.example.hometestnew.jwt;
 
 
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -22,37 +19,32 @@ import java.util.logging.Logger;
 
 public class SecurityConfig {
 //    private static final Logger logger = (Logger) LoggerFactory.getLogger(SecurityConfig.class);
+   
+
+//    // Bean to configure CORS in Spring Security
+//    @Bean
+//    public CorsConfigurationSource customCorsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.addAllowedOrigin("https://hometestjavaprogrammer-production.up.railway.app");
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        configuration.setAllowedHeaders(List.of("*"));
+//        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/registration", "/profile", "/profile/update", "/profile/image",
-                                "/banner", "/services", "/balance", "/topup", "/transaction", "/transaction/history",
-                                "/v1/api-docs", "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
-                                "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
-                                "/webjars/**", "/swagger-ui.html", "/auth-controller/**", "/error")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                )
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(customCorsConfigurationSource()));
-
+                .authorizeRequests()
+                .requestMatchers("/login", "/registration", "/profile", "/profile/update", "/profile/image", "/banner", "/services", "/balance", "/topup", "/transaction", "/transaction/history", "/v1/api-docs", "/v2/api-docs", "/v3/api-docs","/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/auth-controller/**", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf(csrf -> csrf.disable());
         return http.build();
     }
-
-    
-    // private CorsConfigurationSource customCorsConfigurationSource() {
-    //     CorsConfiguration configuration = new CorsConfiguration();
-    //     configuration.addAllowedOrigin("https://hometest-java-program-production.up.railway.app");
-    //     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    //     configuration.setAllowedHeaders(List.of("*"));
-    //     configuration.setAllowCredentials(true);
-
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     source.registerCorsConfiguration("/**", configuration);
-    //     return source;
-    // }
 
 
 }
