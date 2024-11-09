b22372ef5e773a9bf7e407c632e0cd655e6f636c
Ahmad Fauzan <134770763+ahmaadfaauzn2@users.noreply.github.com>
2024-11-09 16:05:55 +0700

    Update TopUpRequest.java

--- src/main/java/com/example/hometestnew/models/TopUpRequest.java
+++ src/main/java/com/example/hometestnew/models/TopUpRequest.java
@@ -1,15 +1,30 @@
 package com.example.hometestnew.models;
 
 
-public class TopUpRequest {
+import io.swagger.v3.oas.annotations.media.Schema;
 
-    private double topUpAmount;
+public class LoginRequest {
 
-    public double getTopUpAmount() {
-        return topUpAmount;
+    @Schema(description = "User email address", example = "user@gmail.com")
+    private String email;
+
+    @Schema(description = "User password", example = "abcdef1234")
+    private String password;
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
     }
 
-    public void setTopUpAmount(double topUpAmount) {
-        this.topUpAmount = topUpAmount;
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
