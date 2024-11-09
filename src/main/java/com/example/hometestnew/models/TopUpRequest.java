0a76f445df1b6f5634752765524277c7e3a785cd
Ahmad Fauzan <134770763+ahmaadfaauzn2@users.noreply.github.com>
2024-11-09 15:57:00 +0700

    Create TopUpRequest.java

---
+++ src/main/java/com/example/hometestnew/models/TopUpRequest.java
@@ -0,0 +1,15 @@
+package com.example.hometestnew.request;
+
+
+public class TopUpRequest {
+
+    private double topUpAmount;
+
+    public double getTopUpAmount() {
+        return topUpAmount;
+    }
+
+    public void setTopUpAmount(double topUpAmount) {
+        this.topUpAmount = topUpAmount;
+    }
+}
