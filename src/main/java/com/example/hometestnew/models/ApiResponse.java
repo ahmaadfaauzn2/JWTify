4e10904a2408d6067543e87c3038c3e1cf9a9b66
Ahmad Fauzan <134770763+ahmaadfaauzn2@users.noreply.github.com>
2024-11-09 15:30:20 +0700

    Update ApiResponse.java

--- src/main/java/com/example/hometestnew/models/ApiResponse.java
+++ src/main/java/com/example/hometestnew/models/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.example.hometestnew.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Map;
 
 public class ApiResponse {
@@ -8,6 +10,28 @@ public class ApiResponse {
     private int status;
     private String message;
     private Map<String, Object> data;
+
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Update to use a custom response structure
+    private String email;
+    private String password;
 
 
 
